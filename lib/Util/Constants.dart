024b32e64ae7dfd0f2827262636e772dbb39e9a1
Mehak Kalia <72802050+mehak-kalia@users.noreply.github.com>
2021-10-19 22:42:03 +0530

    Splash Page

--- lib/Util/Constants.dart
+++ lib/Util/Constants.dart
@@ -3,7 +3,8 @@ import 'package:flutter/cupertino.dart';
 
 class Util {
 
-  static String Logo = "B SAFE.png";
+  static String App_Name = "B SAFE 24X7";
+  static String Logo = "bsafe.png";
   static Color color = Color.fromARGB(255, 255, 96, 125);
 
 }
