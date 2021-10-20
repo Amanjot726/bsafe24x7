ae22660d7ba42cd8e39d860b0f137d62168ed7c7
Priyanka Jhamb <74251229+PriyankaJhamb@users.noreply.github.com>
2021-10-20 18:12:59 +0530

    Update Db_operations.dart

--- lib/Util/Db_operations.dart
+++ lib/Util/Db_operations.dart
@@ -1,4 +1,4 @@
-import 'package:bsafe24x7/Util/constants.dart';
+import 'package:bsafe24x7/Util/Constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
