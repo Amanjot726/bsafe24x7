ac97b3a795e4188280181e7f586f9f9d5dffa1f7
Priyanka Jhamb <74251229+PriyankaJhamb@users.noreply.github.com>
2021-10-20 18:06:14 +0530

    Update Terms_and_conditions.dart

--- lib/Other_Pages/Terms_and_conditions.dart
+++ lib/Other_Pages/Terms_and_conditions.dart
@@ -1,1 +1,69 @@
 
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+
+
+class TnCPage extends StatefulWidget {
+  const TnCPage({Key? key}) : super(key: key);
+
+  @override
+  _TnCPageState createState() => _TnCPageState();
+}
+
+class _TnCPageState extends State<TnCPage> {
+  @override
+  void initState() {
+    super.initState();
+    if(Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: 'https://digital.kecrpg.com/raksha/PrivacyPolicy.html',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+        onProgress: (int progress) {
+          print("WebView is loading (progress : $progress%)");
+        },
+        javascriptChannels: <JavascriptChannel>{
+          _toasterJavascriptChannel(context),
+        },
+        navigationDelegate: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            print('blocking navigation to $request}');
+            return NavigationDecision.prevent;
+          }
+          print('allowing navigation to $request');
+          return NavigationDecision.navigate;
+        },
+        onPageStarted: (String url) {
+          print('Page started loading: $url');
+        },
+        onPageFinished: (String url) {
+          print('Page finished loading: $url');
+        },
+        gestureNavigationEnabled: true,
+      ),
+    );
+  }
+}
+
