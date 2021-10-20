3871c5f7d5b587a9cdb41fd5c83f4d680f692be1
PriyankaJhamb <priyankajhamb73@gmail.com>
2021-10-20 18:18:14 +0530

    imported file of firebase _Core

--- lib/Util/Db_operations.dart
+++ lib/Util/Db_operations.dart
@@ -1,6 +1,6 @@
-import 'package:bsafe24x7/Util/constants.dart';
+import 'package:bsafe24x7/Util/Constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 
 fetchHelplineNumbers() {
   Stream<QuerySnapshot> stream = FirebaseFirestore.instance.collection(Util.HELPLINE_COLLECTION).snapshots();
