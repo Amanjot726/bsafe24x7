8ecd599a46ce319923842319742e3619205b5d7f
Priyanka Jhamb <74251229+PriyankaJhamb@users.noreply.github.com>
2021-10-20 17:53:24 +0530

    Update Constants.dart

--- lib/Util/Constants.dart
+++ lib/Util/Constants.dart
@@ -6,6 +6,6 @@ class Util {
   static String Logo = "assets/bsafe.png"; 
   static Color color = Color.fromARGB(255, 255, 96, 125);
   // static Color color = Color.fromARGB(255, 255, 148, 168);
-
+  static String HELPLINE_COLLECTION = "helpline";
 }
 
